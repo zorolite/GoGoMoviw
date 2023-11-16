56ddeb755b445fa573eecc5d6c6db6d7d8e72840
zorolite <119780530+zorolite@users.noreply.github.com>
2023-11-16 22:44:43 +0530

    Update constants.ts

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,6 +1,6 @@
 export const API_URL = "https://api.themoviedb.org/3";
 export const IMAGE_URL = "https://image.tmdb.org/t/p";
-export const EMBED_URL = "https://2embed.org/embed";
+export const EMBED_URL = "https://animastersplayer.000webhostapp.com/tmdbseries;
 export const EMBED_VIDSRC = "https://vidsrc.me/embed";
 export const EMBED_TO = "https://www.2embed.to/embed/tmdb";
 
